98ce10c2eb3f5f254081daeb430ce2d6140c0287
amuntean <ademdigitbcn@gmail.com>
2023-11-28 22:21:49 +0100

    exercise 3.4

--- index.js
+++ index.js
@@ -24,8 +24,21 @@ let persons = [
       }
 ]
 
+// get all persons
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+// get one person
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/info', (request, response) => {
